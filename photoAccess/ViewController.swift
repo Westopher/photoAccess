9ae458fe1d1fbe30ec05f0f368868e6311ad5b6c
West Kraemer <kraemerwest@gmail.com>
2017-08-30 14:04:03 -0400

    add comment and assign photo chosen to display on imageView

--- photoAccess/ViewController.swift
+++ photoAccess/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            imageView.image = image
+            
+        } else {
+            
+            print("you didn't get the image. don't shoot me, i'm just the messenger")
+            
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
     
     @IBAction func importImage(_ sender: Any) {
         
@@ -17,7 +30,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         
         imagePickerController.delegate = self
         
-        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary //can use .camera here instead of photolibrary if desired
+        
         
         imagePickerController.allowsEditing = false
         
